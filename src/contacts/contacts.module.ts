1a5cef045645bd587c05f22cde88c02fc69dc535
CarlesRa <jcramos8282@gmail.com>
2020-10-28 09:38:07 +0100

    Test Cirleci

--- src/contacts/contacts.module.ts
+++ src/contacts/contacts.module.ts
@@ -2,9 +2,9 @@ import { Module } from '@nestjs/common';
 import { ContactsService } from './services/contacts/contacts.service';
 import { ContactsController } from './controllers/contacts/contacts.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Contact } from './model/contact.entity';
 import { EmailController } from './controllers/email/email.controller';
 import { EmailService } from './services/email/email.service';
+import { Contact } from './model/contacts/contact.entity';
 
 @Module({
 	imports:[
